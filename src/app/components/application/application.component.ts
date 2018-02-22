e900b804fbcd350ea15a17bf7c2bb8f71639f70b
v.savinov <v.savinov@irkpk.ru>
2018-02-22 11:04:56 +0800

    add ViewEncapsulation.None.

--- src/app/components/application/application.component.ts
+++ src/app/components/application/application.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {ProductService} from '../../services/product.service';
 import {Product} from '../../model/product.entity';
 
 @Component({
   selector: 'auction-application',
   templateUrl: './application.component.html',
-  styleUrls: ['./application.component.css']
+  styleUrls: ['./application.component.css'],
+  encapsulation:ViewEncapsulation.None
+
 })
 export class ApplicationComponent implements OnInit {
   products: Array<Product> = [];
