edb978686eaaa02adde92534d7ab44b724e1f8a5
skorpion <savinov_vu@inbox.ru>
2018-02-24 10:09:34 +0800

    add start loading.

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'auction-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
+  encapsulation:ViewEncapsulation.None
+
 })
 export class CarouselComponent implements OnInit {
 
